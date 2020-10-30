d28892b44dc1f8ca4f97366abbb577a3c2b1de96
Naufal Alexander Suryasumirat <naufal.suryasumirat@gmail.com>
2020-10-30 20:57:59 +0700

    test Sastrawi module

--- test.py
+++ test.py
@@ -1,3 +1,20 @@
+# import Sastrawi package
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# create stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+# stem
+sentence = 'Perekonomian Indonesia sedang dalam pertumbuhan yang membanggakan'
+output   = stemmer.stem(sentence)
+
+print(output)
+# ekonomi indonesia sedang dalam tumbuh yang bangga
+
+print(stemmer.stem('Mereka meniru-nirukannya'))
+# mereka tiru
+
 print("Anjay Mabar")
 nama = input("Nama : ")
 print("Halo " + nama)
